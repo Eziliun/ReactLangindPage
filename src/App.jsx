9634407d14fb975143a45b92c6405148526d0ad9
Wilian Dimitri <wilianw2011@hotmail.com>
2024-03-27 17:54:55 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./component/Header";
+import Hero from "./component/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
